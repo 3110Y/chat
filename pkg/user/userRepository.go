3f191300daabca14658874800f9c52f2ffd8e18e
Gaevoy <gaevoyrs@gmail.com>
2022-06-04 06:38:33 +0300

    entity

--- pkg/user/userRepository.go
+++ pkg/user/userRepository.go
@@ -1,9 +1,21 @@
 package user
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/jmoiron/sqlx"
+	"time"
+)
 
 type userRepository struct {
 	DB *sqlx.DB
+}
+
+type User struct {
+	Id        string
+	Name      string
+	Token     string
+	BlockedAt time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 func NewUserRepository(DB *sqlx.DB) *userRepository {
